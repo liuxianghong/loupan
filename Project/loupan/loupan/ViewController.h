cd03208093539f05cac05517715e7c572aa85d95
liuxh532 <786995209@qq.com>
2015-03-25 11:09:20 +0800

    lou pan project

---
+++ Project/loupan/loupan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  loupan
+//
+//  Created by 刘向宏 on 15-3-25.
+//  Copyright (c) 2015年 刘向宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Project/loupan/loupan/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  loupan
+//
+//  Created by 刘向宏 on 15-3-25.
+//  Copyright (c) 2015年 刘向宏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
